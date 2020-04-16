11073e977f6985710762c3859cccbddf91410025
Ryosuke Shirasu <m011518618@edu.teu.ac.jp>
2020-04-16 14:19:52 +0900

    Add files via upload

---
+++ book/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Book::Application.config.secret_token = '904186c599abfa16b21fa497353618466691767e300b508b63d39b9833e75b7c5b1e02eecdb3206696bb5f1e5444ff955d3c8e9e82cf958ed63fa4484b36439c'
